dc202b0ea22774fb4db6316d12726947574f7dca
Shubham Singh <suvamsingh55@gmail.com>
2025-01-21 15:46:00 +0530

    [golang][webapp] a simple web app written in golang

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("A simple web application running on 8080 port")
+	fmt.Fprintln(w, "Hello World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Println("Server starting on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
